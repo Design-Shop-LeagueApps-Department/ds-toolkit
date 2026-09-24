e35a7d4c260cab0eabf10c7aabd0a6f00b7ff7c2
agabriel1590 <agabriel@leagueapps.com>
2026-09-24 20:31:03 +0800

    feat: Heading Style 2 Double Line, independent colours per line (1.9.155) (#227) (#228)
    Separator Line 1 / Line 2 Color pickers with global-colour connections,
    blank = the divider colour, so existing Double Line modules are byte-identical.
    
    Co-authored-by: agabriel1590 <228749420+agabriel1590@users.noreply.github.com>

--- ds-toolkit.php
+++ ds-toolkit.php
@@ -3,7 +3,7 @@
  * Plugin Name:       DS Toolkit
  * Plugin URI:        https://github.com/agabriel1590/ds-toolkit
  * Description:       Design Shop custom features and build toolkit.
- * Version:           1.9.154
+ * Version:           1.9.155
  * Author:            Alipio Gabriel
  * Author URI:        https://github.com/agabriel1590
  * Text Domain:       ds-toolkit
@@ -31,7 +31,7 @@ if ( defined( 'DS_TOOLKIT_VERSION' ) ) {
     return;
 }
 
-define( 'DS_TOOLKIT_VERSION', '1.9.154' );
+define( 'DS_TOOLKIT_VERSION', '1.9.155' );
 define( 'DS_TOOLKIT_PATH', plugin_dir_path( __FILE__ ) );
 define( 'DS_TOOLKIT_URL', plugin_dir_url( __FILE__ ) );
 
